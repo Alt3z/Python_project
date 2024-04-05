0018020eda9b9acd42789b0838076b3ca0043027
Alt3z <166149674+Alt3z@users.noreply.github.com>
2024-04-05 21:50:37 +0500

    Update main.py

--- Функция для обработки данных, хранящихся в текстовых файлах/main.py
+++ Функция для обработки данных, хранящихся в текстовых файлах/main.py
@@ -1,19 +1,19 @@
 import os
 
-def MaxValue(): # функция для поиска максимального значения во втором столбце всех фалов в директории
-    OutFile = open("out.txt", "w")
+def MaxValue(): # Функция для поиска максимального значения во втором столбце всех фалов в директории
+    Out_File = open("out.txt", "w")
 
-    for filename in os.listdir("D:/8 трим/python/test"): # Перебираем все файлы в директории
-        with open(os.path.join("D:/8 трим/python/test", filename), 'r') as f: # открываем файл
-            MaxValue = None
+    for filename in os.listdir("путь к вашей директории"): # Перебираем все файлы в директории
+        with open(os.path.join("D:/8 трим/python/test", filename), 'r') as f: # Открываем файл
+            Max_Value = None
             for line in f:
                 column = line.split() # Разделяем строку по пробелам
                 value = float(column[1].replace('D', 'E'))
-                if MaxValue is None or value > MaxValue:
-                    MaxValue = value
+                if Max_Value is None or value > Max_Value:
+                    Max_Value = value
 
             name = filename[filename.find(".") + 1:]
-            name = name.rstrip("D+04.dat") + "     " + str(MaxValue)
-            OutFile.write(name + '\n')
+            name = name.rstrip("D+04.dat") + "     " + str(Max_Value)
+            Out_File.write(name + '\n')
 
 MaxValue()
